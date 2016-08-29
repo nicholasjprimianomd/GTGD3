c7fa1a5bda27fcdada5a9af3f621f6596b0b8423
Nicholas Primiano <njp280@nyu.edu>
2016-08-28 22:12:54 -0400

    no message

---
+++ Assets/Scripts/Chapter1/TriggerExample.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Chapter1
+{
+	public class TriggerExample : MonoBehaviour
+	{
+
+		private EventMaster eventMasterScript;
+
+		void SetInitalReferances ()
+		{
+			eventMasterScript = GameObject.Find ("GameManager").GetComponent<EventMaster> ();
+		}
+
+		void Start ()
+		{
+			SetInitalReferances ();
+		}
+
+		void OnTriggerEnter (Collider other)
+		{
+
+			eventMasterScript.CallMyGeneralEvent ();
+			Destroy (gameObject);
+
+		}
+			
+	}
+}
---
+++ Assets/Scripts/Chapter1/WalkThroughWall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Chapter1
+{
+	public class WalkThroughWall : MonoBehaviour
+	{
+		private Color myColor = new Color (.5f, 1f, 0.5f, .3f);
+		private EventMaster eventMasterScript;
+
+		void SetLayerToNotSolid ()
+		{
+			gameObject.layer = LayerMask.NameToLayer ("NotSolid");
+			GetComponent<Renderer> ().material.SetColor ("_Color", myColor);
+		}
+
+		void OnEnable ()
+		{
+			SetInitialRenferances ();
+			eventMasterScript.myGeneralEvent += SetLayerToNotSolid;
+		}
+
+		void OnDisable ()
+		{
+			eventMasterScript.myGeneralEvent -= SetLayerToNotSolid;
+		}
+
+		void SetInitialRenferances ()
+		{
+			eventMasterScript = GameObject.Find ("GameManager").GetComponent<EventMaster> ();
+		}
+
+	}
+}
