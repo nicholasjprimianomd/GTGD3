ce7a3b6ddd97dc39dbae3fd0c0d72c791b5ef994
Nicholas Primiano <njp280@nyu.edu>
2016-08-29 14:23:13 -0400

    no message

---
+++ Assets/Scripts/MasterScripts/GameManager_Master.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace S3
+{
+	public class GameManager_Master : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	}
+}
